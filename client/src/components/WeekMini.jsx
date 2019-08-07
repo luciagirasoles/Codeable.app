99a9579fcce319874fca2c60a6f0c5c99d76b210
Mayra Lucia Navarro <luciagirasoles@gmail.com>
2019-08-06 22:49:45 -0500

    Create responsive view for Miniassigment Grades (#76)
    * remove unnecessary data file
    
    * improve responsive
    
    * create responsive view miniassigments
    
    * create responsive for miniassignments grades view

--- client/src/components/WeekMini.jsx
+++ client/src/components/WeekMini.jsx
@@ -6,21 +6,40 @@ import { Card } from "./UI/Ui";
 function WeekMini({ title, score }) {
   const rowStyle = {
     display: "flex",
-    textAlign: "center",
+    textAlign: "inherit",
     justifyContent: "space-between",
-    margin: "5px 0"
+    margin: "15px 0",
+    "@media (max-width: 760px)": {
+      margin: "15px 0"
+    }
   };
 
   return (
     <>
-      <Card css={{ marginBottom: "1rem", width: "40%" }}>
+      <Card
+        css={{
+          marginBottom: "1rem",
+          width: "40%",
+          "@media (max-width: 760px)": {
+            width: "90%"
+          }
+        }}
+      >
         <h2>{title}</h2>
         <section css={{ marginTop: "15px" }}>
           {score.map(value => {
             return (
               <div css={rowStyle}>
                 <span>{value.title}</span>
-                <span>{value.score}</span>
+                <span
+                  css={{
+                    display: "flex",
+                    flexDirection: "column-reverse",
+                    marginLeft: "10px"
+                  }}
+                >
+                  {value.score}
+                </span>
               </div>
             );
           })}
